5c8c87b26fd0692543e6fe428a34c3d170060caa
Partha Pratim Sirker <partha.sirker@climate.com>
2017-01-20 14:24:23 -0800

    maxa alternating sequence

---
+++ src/com/dsalgo/MaxAlternatingSequence.java
@@ -0,0 +1,23 @@
+package com.dsalgo;
+
+public class MaxAlternatingSequence {
+
+	public static void main(String[] args) {
+		int[] arr={5,3,4,6,7,1,2,8,7,5,7,4,6,3,4,2,8,9,8,9,6,8,5,7};
+		
+
+	}
+	public static int[]getLongestAlternatingSubsequence(int[]arr){
+		int startIndex = 0;
+		int endIndex = 0;
+		int maxStartIndex = 0;
+		int maxEndIndex = 0;
+		int maxLength = 0;
+		for(int i=0;i<arr.length-1;++i){
+		}
+		
+		
+		return new int[]{-1};
+	}
+
+}
